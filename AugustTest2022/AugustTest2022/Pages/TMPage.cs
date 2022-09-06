ddae7d31a9f3aca416b4cc434770687961e338e0
joey <joeymagpantay3@gmail.com>
2022-09-07 09:41:43 +1200

    added asssertion

--- AugustTest2022/AugustTest2022/Pages/TMPage.cs
+++ AugustTest2022/AugustTest2022/Pages/TMPage.cs
@@ -1,4 +1,5 @@
 ﻿using AugustTest2022.Utilities;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
@@ -47,22 +48,13 @@ namespace AugustTest2022.Pages
             WaitHelpers.WaitToBeClickable(driver, "XPath", "//*[@id=\'tmsGrid\']/div[4]/a[4]", 3);
             lastPageButton.Click();
             IWebElement newCodeName = driver.FindElement(By.XPath("//*[@id=\'tmsGrid\']/div[3]/table/tbody/tr[last()]/td[1]")); ;
-
-            if (newCodeName.Text == "Marcos")
-            {
-                Console.WriteLine("Record Successfully Created");
-            }
-            else
-            {
-                Console.WriteLine("Can't Find Record");
-            }
-
+            Assert.That(newCodeName.Text == "Marcos", "Can't Find Record");
         }
 
         public void EditTM(IWebDriver driver)
         {
-            IWebElement lastPageButton = driver.FindElement(By.XPath("//*[@id=\'tmsGrid\']/div[4]/a[4]"));
             WaitHelpers.WaitToBeClickable(driver, "XPath", "//*[@id=\'tmsGrid\']/div[4]/a[4]", 3);
+            IWebElement lastPageButton = driver.FindElement(By.XPath("//*[@id=\'tmsGrid\']/div[4]/a[4]"));
             lastPageButton.Click();
             // Click on edit button
             WaitHelpers.WaitToBeClickable(driver, "XPath", "//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[5]/a[1]", 3);
@@ -90,10 +82,10 @@ namespace AugustTest2022.Pages
             //Save time and material
             IWebElement saveEditedTimeAndMaterial = driver.FindElement(By.Id("SaveButton"));
             saveEditedTimeAndMaterial.Click();
-            Thread.Sleep(1000);
+            Thread.Sleep(3000);
 
             // Check if record is edited
-            WaitHelpers.WaitToBeClickable(driver, "XPath", "//*[@id='tmsGrid']/div[4]/a[4]", 3);
+            WaitHelpers.WaitToBeClickable(driver, "XPath", "//*[@id=\'tmsGrid\']/div[4]/a[4]", 5);
             IWebElement lastPageButton2 = driver.FindElement(By.XPath("//*[@id=\'tmsGrid\']/div[4]/a[4]"));
             lastPageButton2.Click();
             IWebElement newEditedCode = driver.FindElement(By.XPath("//*[@id=\'tmsGrid\']/div[3]/table/tbody/tr[last()]/td[1]"));
@@ -108,12 +100,14 @@ namespace AugustTest2022.Pages
             {
                 Console.WriteLine("Can't Find Record");
             }
+
         }
 
         public void DeleteTM(IWebDriver driver)
         {
 
             // Delete time record
+            WaitHelpers.WaitToBeClickable(driver, "XPath", "//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[5]/a[2]", 3);
             IWebElement deleteButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[5]/a[2]"));
             deleteButton.Click();
             driver.SwitchTo().Alert().Accept();
